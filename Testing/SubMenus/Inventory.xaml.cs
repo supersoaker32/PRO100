080eb447e5d8bdedc551487ac2c9cacfd7f5117d
supersoaker32 <supersoaker200032@gmail.com>
2019-11-07 16:03:39 -0700

    Added inventory display and add item box and button

--- Testing/SubMenus/Inventory.xaml.cs
+++ Testing/SubMenus/Inventory.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,32 @@ namespace Testing.SubMenus
         public Inventory()
         {
             this.InitializeComponent();
+
+        }
+
+        private void inputEntered_Click(object sender, RoutedEventArgs e)
+        {
+            //Create new shape and brush
+            TextBlock item = new TextBlock();
+            SolidColorBrush color = new SolidColorBrush();
+
+            //Set color
+            color.Color = Colors.Black;
+            item.Foreground = color;
+            item.FontSize = 55;
+            if(input.Text.Trim() != "")
+            {
+                Grid grid = new Grid();
+                SolidColorBrush brush = new SolidColorBrush();
+                brush.Color = Colors.DarkGray;
+                grid.BorderBrush = brush;
+                grid.BorderThickness = new Thickness(3);
+                grid.CornerRadius = new CornerRadius(8);
+                item.Text = input.Text;
+                grid.Children.Add(item);
+                inventory.Children.Add(grid);
+                input.Text = "";
+            }
         }
     }
 }
