c37af80bd5770051f74b6a407c1039e793f85c1b
Vernon Howerton <vhowerton@student.neumont.edu>
2019-11-07 14:12:58 -0700

    Added using for submenus

--- Testing/MainPage.xaml.cs
+++ Testing/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Testing.SubMenus;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.ViewManagement;
