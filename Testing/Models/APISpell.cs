ecb0430de299d7725bad2ef0c74b5b5a4ce4cf3a
supersoaker32 <supersoaker200032@gmail.com>
2019-11-24 12:49:10 -0700

    Implemented spell API Spells are autofilled when typing names, but custom names are also possible Once a spell name is selected, if there's a prerecorded spell name, the description is autofilled, but can be changed as well

---
+++ Testing/Models/APISpell.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.Models
+{
+    public class School
+    {
+        public string url { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Class
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Subclass
+    {
+        public string url { get; set; }
+        public string name { get; set; }
+    }
+
+    public class RootSpell
+    {
+        public string _id { get; set; }
+        public int index { get; set; }
+        public string name { get; set; }
+
+        private List<string> desc;
+        public List<string> Desc
+        {
+            get { return desc; }
+            set
+            {
+                for (int i = 0; i < value.Count; i++)
+                {
+                    value[i] = value[i].Replace("â€™", "'");
+                    value[i] = value[i].Replace("™", "");
+                    value[i] = value[i].Replace("€", "");
+                    value[i] = value[i].Replace("â", "");
+                    value[i] = value[i].Replace("�", "");
+                    value[i] = value[i].Replace("œ", "");
+                }
+                desc = value;
+            }
+        }
+
+        private List<string> higher_level;
+        public List<string> Higher_Level
+        {
+            get { return higher_level; }
+            set
+            {
+                for (int i = 0; i < value.Count; i++)
+                {
+                    value[i] = value[i].Replace("â€™", "'");
+                }
+                higher_level = value;
+            }
+        }
+        public string page { get; set; }
+        public string range { get; set; }
+        public List<string> components { get; set; }
+
+        private string material;
+
+        public string Material
+        {
+            get { return material; }
+            set { material = value.Replace("â€™", "'"); }
+        }
+
+        public string ritual { get; set; }
+        public string duration { get; set; }
+        public string concentration { get; set; }
+        public string casting_time { get; set; }
+        public int level { get; set; }
+        public School school { get; set; }
+        public List<Class> classes { get; set; }
+        public List<Subclass> subclasses { get; set; }
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            string returnString;
+
+            string description = "";
+            foreach (string descript in desc)
+            {
+                description += descript;
+            }
+
+            string higherLevel = "";
+            if (higher_level != null)
+            {
+                foreach (string highlevel in higher_level)
+                {
+                    higherLevel += highlevel;
+                }
+            }
+
+            string componentsDisplay = "";
+            foreach (string component in components)
+            {
+                componentsDisplay += component;
+            }
+
+            string classesDisplay = "";
+            foreach (Class classInClasses in classes)
+            {
+                classesDisplay += classInClasses.name + "\n\t";
+            }
+            returnString =
+                $"Name: {name}\n" +
+                $"Description: {description}\n" +
+                $"Higher Level: {higherLevel}\n" +
+                $"Page: {page}\n" +
+                $"Range: {range}\n" +
+                $"Components: {componentsDisplay}\n" +
+                $"Material: {material}\n" +
+                $"Ritual: {ritual}\n" +
+                $"Duration: {duration}\n" +
+                $"Concentration: {concentration}\n" +
+                $"Level: {level}\n" +
+                $"School: {school.name}\n" +
+                $"Classes:   {classesDisplay}";
+            return returnString;
+        }
+    }
+}
---
+++ Testing/Models/APISpellDictionary.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Testing.Models
+{
+    public class APISpellDictionary
+    {
+
+        List<RootSpell> spells = new List<RootSpell>();
+        List<string> spellNames = new List<string>();
+        List<string> spellDescriptions = new List<string>();
+
+        public List<string> SpellNames
+        {
+            get { return spellNames; }
+            set { spellNames = value; }
+        }
+
+        public List<string> SpellDescriptions
+        {
+            get { return spellDescriptions; }
+            set { spellDescriptions = value; }
+        }
+
+
+        public APISpellDictionary()
+        {
+            InitializeDictionary();
+        }
+
+        public async void InitializeDictionary()
+        {
+            for (int i = 1; i < 319; i++)
+            {
+                //Initializes view of all spells in order
+                Task t = Task.Run( () => JsonCall(i));
+                t.Wait();
+            }
+        }
+
+        public async Task JsonCall(int index)
+        {
+            //Retrieves a spell by index
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(new Uri($"http://dnd5eapi.co/api/spells/{index}/"));
+            var json = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<RootSpell>(json);
+            spells.Add(result);
+            spellNames.Add(result.name);
+            string description = "";
+            for(int i = 0; i < result.Desc.Count; i++)
+            {
+                description += result.Desc.ElementAt(i);
+            }
+            spellDescriptions.Add(description);
+        }
+    }
+}
